2608a5e071d2e8a2a0d5ab3ace4b139c7a11a248
McDirty <wiredmilk@gmail.com>
2015-05-13 11:42:30 -0700

    Updated to callback structure for player

--- lrgmaster.js
+++ lrgmaster.js
@@ -14,8 +14,6 @@ function playMovie(filename){
     moviePlaying = filename;
 }
 
-omx.play(moviePlaying, {loop: true});
-
 setInterval(function() {
         if(moviePlaying == 'madmax.mp4'){
             //omx play movie in ping pong fashion

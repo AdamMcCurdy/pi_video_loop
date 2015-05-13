54bc508ea29c36699c52d0e29a8ed833d9f374a5
Adam <wiredmilk@gmail.com>
2015-05-13 17:51:57 +0000

    updated pi player

--- lrgmaster.js
+++ lrgmaster.js
@@ -2,11 +2,21 @@
  * Created by Adam on 5/13/15.
  */
 var omx = require('omxcontrol');
-var secondsToWait = 5;
+var secondsToWait = 10;
+var moviePlaying = "madmax";
 
 setInterval(function() {
-        console.log('test');
-        //omx play movie in ping pong fashion
-        //send signal to other pi about playing next movie
+ 	if(moviePlaying == 'madmax'){
+		//omx play movie in ping pong fashion
+		omx.quit();
+		omx.start('avengers.mp4');
+       		moviePlaying = 'avengers';
+		//send signal to other pi about playing next movie
+	}
+	else if(moviePlaying == 'avengers'){
+		omx.quit();
+		omx.start('madmax.mp4');
+		moviePlaying = 'madmax';
+	}
     }, secondsToWait * 1000
 )

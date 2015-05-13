04e6d8540cd0a6ffc483a0e42c57615b0fa7e8b0
McDirty <wiredmilk@gmail.com>
2015-05-13 10:57:06 -0700

    updated to omxdirector

--- lrgmaster.js
+++ lrgmaster.js
@@ -1,21 +1,21 @@
 /**
  * Created by Adam on 5/13/15.
  */
-var omx = require('omxcontrol');
-var secondsToWait = 10;
+var omx = require('omxdirector');
+var secondsToWait = 30;
 var moviePlaying = "madmax";
 
 setInterval(function() {
  	if(moviePlaying == 'madmax'){
 		//omx play movie in ping pong fashion
-		omx.quit();
-		omx.start('avengers.mp4');
+		omx.stop();
+		omx.start('avengers.mp4', {loop: true});
        		moviePlaying = 'avengers';
 		//send signal to other pi about playing next movie
 	}
 	else if(moviePlaying == 'avengers'){
-		omx.quit();
-		omx.start('madmax.mp4');
+		omx.stop();
+		omx.start('madmax.mp4', {loop: true});
 		moviePlaying = 'madmax';
 	}
     }, secondsToWait * 1000

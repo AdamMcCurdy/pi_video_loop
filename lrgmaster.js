a2e17c50ef9b20e8897f7ce94257ca780604fa3d
McDirty <wiredmilk@gmail.com>
2015-05-31 00:59:10 -0700

    Last push for the night

--- lrgmaster.js
+++ lrgmaster.js
@@ -2,11 +2,14 @@
  * Created by Adam on 5/13/15.
  */
 var omx = require('omxdirector')
+    , usb = require('usb')
+    , triggeredTime
  //, secondsToWait = 30
-    , moviePlaying = 'farClip.mp4'
+    , moviePlaying = 'trigger.mp4'
     , avgData = []
     , sampleSize = 25
-    , sensorMin = 299;
+    , sensorMin = 299
+    , startDevices = [];
 
 var SerialPort = require("serialport").SerialPort;
 var serialPort = new SerialPort("/dev/tty.usbserial-MBY0W12V", { // mac version
@@ -14,19 +17,28 @@ var serialPort = new SerialPort("/dev/tty.usbserial-MBY0W12V", { // mac version
     baudrate: 57600
 });
 
+function loadMoviesFromDir(usbPath){
+
+}
+function inventoryUSBDevices(){
+
+}
+
+console.log('length is ' +  usb.getDeviceList().length);
+
 function playMovie(filename){
-    //omx.stop();
-    //setTimeout(function(){
-    //    omx.play(filename, {loop: true});
-    //}, 500);
+    omx.stop();
+    setTimeout(function(){
+        omx.play(filename, {loop: true});
+    }, 500);
 
     moviePlaying = filename;
 }
-console.log("Playing " + moviePlaying);
-//playMovie(moviePlaying);
+//console.log("Playing " + moviePlaying);
+playMovie(moviePlaying);
 
 serialPort.on("open", function () {
-    console.log('open');
+    //console.log('open');
     serialPort.on('data', function(data) {
         data = data.toString().split('R')[1];
         if(data > sensorMin){
@@ -42,14 +54,15 @@ serialPort.on("open", function () {
 
             var average = sum / avgData.length;
             average = Math.round(average);
-            console.log(average);
-            if(data < 1500 && moviePlaying == 'farClip.mp4'){
+            //console.log(average);
+            //console.log(data);
+            if(data < 1500 && moviePlaying == 'main.mp4'){
                 //omx play movie in ping pong fashion
-                playMovie('nearClip.mp4');
+                playMovie('trigger.mp4');
                 //send signal to other pi about playing next movie
             }
             if(data > 1500 && moviePlaying == 'nearClip.mp4'){
-                playMovie('farClip.mp4');
+                playMovie('main.mp4');
             }
         }
     });

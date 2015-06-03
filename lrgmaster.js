e038f9749b50a236a412ceb61a3332c69754583f
McDirty <wiredmilk@gmail.com>
2015-06-02 23:52:18 -0400

    retail step 1

--- lrgmaster.js
+++ lrgmaster.js
@@ -14,17 +14,18 @@ var omx = require('omxdirector')
     , sensorMin = 299
     , startDevices = [];
 
-//var SerialPort = require("serialport").SerialPort;
+var SerialPort = require("serialport").SerialPort;
 //var serialPort = new SerialPort("/dev/tty.usbserial-MBY0W12V", { // mac version
-////var serialPort = new SerialPort("/dev/ttyUSB0", { //pi version
-//    baudrate: 57600
-//});
-setInterval(sampleUSBReading, 1000);
+var serialPort = new SerialPort("/dev/ttyUSB0", { //pi version
+    baudrate: 57600
+});
+
+//setInterval(sampleUSBReading, 1000);
 setInterval(checkUSBReading, 500);
 
 function checkUSBReading(){
     //check reading and also whether movie already playing
-    if(lastReading < 1000 && triggered == false){
+    if(lastReading < 1500 && triggered == false){
 
         //checking to see if current time is under 30 seconds
         if(time.time() - triggeredTime > timeToTrigger){
@@ -73,35 +74,36 @@ function playMovie(filename){
 }
 //console.log("Playing " + moviePlaying);
 
-//playMovie(moviePlaying);
+playMovie(moviePlaying);
 
-//serialPort.on("open", function () {
-//    //console.log('open');
-//    serialPort.on('data', function(data) {
-//        data = data.toString().split('R')[1];
-//        if(data > sensorMin){
-//            if(avgData.length >= sampleSize){
-//                //console.log("removing data");
-//                avgData.shift();
-//            }
-//            avgData.push(data);
-//            var sum = 0;
-//            for(var x = 0; x < avgData.length; x++){
-//                sum = sum + parseInt(avgData[x]);
-//            }
-//
-//            var average = sum / avgData.length;
-//            average = Math.round(average);
-//            //console.log(average);
-//            //console.log(data);
-//            if(data < 1500 && moviePlaying == 'main.mp4'){
-//                //omx play movie in ping pong fashion
-//                playMovie('trigger.mp4');
-//                //send signal to other pi about playing next movie
-//            }
-//            if(data > 1500 && moviePlaying == 'nearClip.mp4'){
-//                playMovie('main.mp4');
-//            }
-//        }
-//    });
-//});
+serialPort.on("open", function () {
+    //console.log('open');
+    serialPort.on('data', function(data) {
+        data = data.toString().split('R')[1];
+        if(data > sensorMin){
+            if(avgData.length >= sampleSize){
+                //console.log("removing data");
+                avgData.shift();
+            }
+            avgData.push(data);
+            var sum = 0;
+            for(var x = 0; x < avgData.length; x++){
+                sum = sum + parseInt(avgData[x]);
+            }
+
+            var average = sum / avgData.length;
+            average = Math.round(average);
+            lastReading = average;
+            //console.log(average);
+            //console.log(data);
+            //if(data < 1500 && moviePlaying == 'main.mp4'){
+            //    //omx play movie in ping pong fashion
+            //    playMovie('trigger.mp4');
+            //    //send signal to other pi about playing next movie
+            //}
+            //if(data > 1500 && moviePlaying == 'nearClip.mp4'){
+            //    playMovie('main.mp4');
+            //}
+        }
+    });
+});

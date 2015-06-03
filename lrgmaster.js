c2093fed2dc046971653aff96863fb13e6026a37
McDirty <wiredmilk@gmail.com>
2015-06-03 03:16:03 -0400

    retail step 1

--- lrgmaster.js
+++ lrgmaster.js
@@ -1,8 +1,13 @@
 /**
  * Created by Adam on 5/13/15.
+ * TODO: List ports and parse through them for usb stick and sensor
+ * TODO: play usb files instead of local ones
  */
-var omx = require('omxdirector')
+
+
+var   omx = require('omxdirector')
     , time = require('time')
+    , usb = require('usb')
     , timeToTrigger = 30
     , lastReading = 5000
     , triggered = false
@@ -11,36 +16,31 @@ var omx = require('omxdirector')
     , minSensorRange = 501
     , maxSensorRange = 3000;
 
-var SerialPort = require("serialport").SerialPort;
-var serialPort = new SerialPort("/dev/ttyUSB0", { //pi version
+var port = require("serialport");
+var SerialPort = port.SerialPort;
+var serialPort = new SerialPort("/dev/ttyUSB1", { //pi version
     baudrate: 57600
 });
 
-setInterval(checkUSBReading, 500);
-
+function clearScreen(){
+    console.log(" ");
+}
 function checkUSBReading(){
     //check reading and also whether movie already playing
     if(lastReading < maxSensorRange && lastReading > minSensorRange && triggered == false){
         //checking to see if current time is under 30 seconds
         if(time.time() - triggeredTime > timeToTrigger){
             playMovie("trigger.mp4");
-            console.log("Triggered");
+            //console.log("Triggered");
             triggeredTime = time.time();
-            console.log("Time = " + triggeredTime);
+            //console.log("Time = " + triggeredTime);
             triggered = true;
         }
-        else{
-            console.log("Too soon");
-        }
-
-    }
-    else{
-        console.log(lastReading);
     }
 
     if(time.time() - triggeredTime > timeToTrigger && triggered == true){
         triggered = false;
-        console.log("Trigger reset");
+        //console.log("Trigger reset");
         playMovie("attract.mp4");
     }
 }
@@ -48,11 +48,9 @@ function checkUSBReading(){
 
 function playMovie(filename){
     omx.stop();
-
     setTimeout(function(){
         omx.play(filename, {loop: true});
     }, 750);
-
     moviePlaying = filename;
 }
 
@@ -71,4 +69,9 @@ serialPort.on("open", function () {
     });
 });
 
+console.log(usb.getDeviceList());
+
 init();
+
+setInterval(checkUSBReading, 500);
+setInterval(clearScreen, 10);
